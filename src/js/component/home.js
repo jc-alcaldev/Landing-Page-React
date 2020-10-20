8a45c016414ed12c90594ca5ac2923ddef9bfca7
jc-alcaldev <carlos_juan1812@hotmail.com>
2020-10-20 14:34:42 +0000

    Ficheros creados

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -7,18 +7,15 @@ import rigoImage from "../../img/rigo-baby.jpg";
 export function Home() {
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<NavBar>n</NavBar>
+			<div className="d-flex row text-align-center">
+				<header>h</header>
+				<Jumbotton className="col-2">c1</Jumbotton>
+				<Jumbotton className="col-2">c2</Jumbotton>
+				<Jumbotton className="col-2">c3</Jumbotton>
+				<Jumbotton className="col-2">c4</Jumbotton>
+			</div>
+			<footer>f</footer>
 		</div>
 	);
 }
